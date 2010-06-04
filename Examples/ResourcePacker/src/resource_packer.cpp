9577045b3b276630bde841d7e8a4b522518143b0
Sirius <siriusnick@gmail.com>
2010-06-05 01:09:59 +0400

    Resource packer added

---
+++ Examples/ResourcePacker/src/resource_packer.cpp
@@ -0,0 +1,61 @@
+﻿/***************************************************************************
+ *   Copyright (C) 2008 by SIRIUS										   *
+ *   SiriusStarNick@yandex.ru											   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program;					                           *
+ ***************************************************************************/
+#include "nova_example_application.h"
+
+using namespace nova;
+
+class ResourcePackerTool : public CExampleApplication
+{
+protected:
+
+public:
+
+	ResourcePackerTool()
+	{
+	}
+
+	~ResourcePackerTool()
+	{
+	}
+
+	void Start()
+	{
+
+	}
+};
+
+
+ENTRY_POINT	
+{
+	try
+	{
+		ResourcePackerTool packer;
+		packer.Start();
+	}
+	catch(NovaExp & exp)
+	{
+		cerr << exp.Message() << endl;
+		exp.FixError();
+	}
+	catch(exception & exp)
+	{
+		cerr << exp.what() << endl;
+	}
+
+	return 0;
+}
