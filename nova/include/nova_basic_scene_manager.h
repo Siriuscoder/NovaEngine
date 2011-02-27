fe7b5439f422e3812df958278d39d09d4d8e3134
Sirius <siriusnick@gmail.com>
2011-02-27 14:39:17 +0300

    Scene manager refactor v2

---
+++ nova/include/nova_basic_scene_manager.h
@@ -0,0 +1,112 @@
+/***************************************************************************
+ *   Copyright (C) 2011 by Sirius										   *
+ *	 Vdov Nikita Sergeevich	(c)											   *
+ *	 siriusnick@gmail.com												   *
+ *																		   *
+ *	 This source file is part of NovaEngine								   *
+ *	 (Object-oriented Game Development Engine)							   *
+ *																		   *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program;					                           *
+ ***************************************************************************/
+#pragma once
+
+#include "nova_scene_manager.h"
+#include "nova_material.h"
+#include "nova_hardware_texture_buffer.h"
+#include "nova_hardware_vertex_buffer.h"
+#include "nova_movable_object.h"
+#include "nova_math.h"
+#include "nova_matrix3d.h"
+#include "nova_matrix4d.h"
+
+namespace nova
+{
+
+
+class NOVA_EXPORT CBasicSceneNode : public CSceneNode
+{
+public:
+
+	typedef struct _batch_st
+	{
+		CHardwareVertexBufferPtr Vbo;
+		CHardwareIndexBufferPtr VboIndex;
+
+		CMaterialPtr Material;
+		nInt32 start_index;
+		nInt32 end_index;
+	} TBatchStruct;
+
+	typedef stl<TBatchStruct>::vector TBatchList;
+
+protected:
+
+	TBatchList mBatchList;
+	CMeshBoxPtr mMeshBox;
+
+	void ValidateNodeImpl(void);
+	void ReleaseNodeImpl(void);
+	void BuildNodeImpl(void);
+	void InValidateNodeImpl(void);
+	void RenderNodeImpl(void);
+	void PrepareNodeImpl(void);
+
+
+	void PreparingBatchList(void);
+
+public:
+
+	CBasicSceneNode(CMovableObject *pObj, CSceneManager *scene);
+
+	~CBasicSceneNode();
+
+	void SetMeshBox(CMeshBoxPtr &mesh);
+
+	inline CMeshBoxPtr GetMeshBox(void) { return mMeshBox; }
+};
+
+
+class NOVA_EXPORT CBasicSceneManager : public CSceneManager
+{
+protected:
+
+	nInt32 RenderSceneImpl(void);
+
+	void PrepareSceneImpl(void);
+
+	nInt32 PrepareRenderQueueImpl(void);
+
+	void PrepareSceneFrameImpl(void);
+
+	void BuildSceneImpl(void);
+
+	void ClearObjectsImpl(void);
+
+	nInt32 RenderNode(CTreeNode<CSceneNode*> *node);
+
+	void PrepareNode(CTreeNode<CSceneNode*> *node);
+
+	//stl<nstring, std::pair<CBasicSceneNode *, CMeshBoxPtr>>::map mObjectsMap;
+
+public:
+
+	CBasicSceneManager(const nstring &scene_name, const nstring & factory_name);
+
+	~CBasicSceneManager() {}
+
+	CSceneNode *AddRenderableResourceToScene(const nstring &resource_name);
+
+};
+
+}
---
+++ nova/src/nova_basic_scene_manager.cpp
@@ -0,0 +1,131 @@
+/***************************************************************************
+ *   Copyright (C) 2011 by Sirius										   *
+ *	 Vdov Nikita Sergeevich	(c)											   *
+ *	 siriusnick@gmail.com												   *
+ *																		   *
+ *	 This source file is part of NovaEngine								   *
+ *	 (Object-oriented Game Development Engine)							   *
+ *																		   *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program;					                           *
+ ***************************************************************************/
+#include "nova_stable_precompiled_headers.h"
+
+#include "nova_basic_scene_manager.h"
+
+namespace nova
+{
+
+
+CBasicSceneNode::CBasicSceneNode(CMovableObject *pObj, CSceneManager *scene) :
+	CSceneNode(pObj, scene)
+{
+
+}
+
+void CBasicSceneNode::SetMeshBox(CMeshBoxPtr &mesh)
+{
+	mMeshBox = mesh;
+}
+
+nInt32 CBasicSceneManager::RenderSceneImpl(void)
+{
+	return RenderNode(GetRootElement());
+}
+
+void CBasicSceneManager::PrepareNode(CTreeNode<CSceneNode*> *node)
+{
+	if(node)
+	{
+		for(nInt32 i = 0; i < node->GetChildrenLen(); i++)
+		{
+			CBasicSceneNode *curNode = dynamic_cast<CBasicSceneNode *>(node->GetData());
+			// Preparing mesh
+			// Generating normals to faces and sub mats info
+			curNode->GetMeshBox()->GenerateNormalsToFaces();
+			if(curNode->GetMeshBox()->GetMeshDefinition().nNormalList.size() == 0)
+			//Generating normals to vertexes
+				curNode->GetMeshBox()->CalculateNormals();
+
+			// Sorting faces by material id
+			// using fast qsort algorithm
+			curNode->GetMeshBox()->SortFaceIndexByMaterials();
+
+			// next layer
+			PrepareNode(node->GetNode(i));
+		}
+	}
+}
+
+
+void CBasicSceneManager::PrepareSceneImpl(void)
+{
+	PrepareNode(GetRootElement());
+}
+
+nInt32 CBasicSceneManager::PrepareRenderQueueImpl(void)
+{
+
+	return 0;
+}
+
+void CBasicSceneManager::PrepareSceneFrameImpl(void)
+{
+
+}
+
+void CBasicSceneManager::BuildSceneImpl(void)
+{
+
+}
+
+void CBasicSceneManager::ClearObjectsImpl(void)
+{
+
+}
+
+nInt32 CBasicSceneManager::RenderNode(CTreeNode<CSceneNode*> *node)
+{
+	nova::nUInt32 nodesRenderedCount = 0;
+	if(node)
+	{
+		for(nInt32 i = 0; i < node->GetChildrenLen(); i++)
+		{
+			node->GetData()->ValidateNode();
+			node->GetData()->RenderNode();
+			nodesRenderedCount++;
+
+			RenderNode(node->GetNode(i));
+			node->GetData()->InValidateNode();
+		}
+	}
+
+	return nodesRenderedCount;
+}
+
+CSceneNode *CBasicSceneManager::AddRenderableResourceToScene(const nstring &resource_name)
+{
+	CMeshBoxPtr mesh = CMeshManager::GetSingelton().GetResourceFromHash(resource_name);
+	if(!mesh.IsNull())
+	{
+		CMovableObject *obj = new CMovableObject(mesh->GetResName(), GetSceneSlavesGroup());
+		CBasicSceneNode *target = new CBasicSceneNode(obj, this);
+		target->SetMeshBox(mesh);
+
+		return target;
+	}
+
+	return NULL;
+}
+
+}
